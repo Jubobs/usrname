ed8683711aa1d48aed45df30fa639b3e842a58e4
Julien Cretel <j.cretel@umail.ucc.ie>
2018-06-26 15:55:02 +0100

    Simplify main

--- main.go
+++ main.go
@@ -21,15 +21,16 @@ func main() {
 
 	// see https://www.safaribooksonline.com/library/view/ultimate-go-programming/9780134757476/ugpg_04_10_03_00.html
 	// and https://gist.github.com/Jubobs/3987c6f9f902489356ccd12422c64e1d
-	ch := make(chan string, len(checkers))
-	waitChecks := len(checkers)
+	n := len(checkers)
+	ch := make(chan string, n)
+	waitChecks := n
 
 	for _, checker := range checkers {
 		go func(c sites.NameChecker) {
-			if err := c.Check(client, username); err != nil && sites.IsUnavailableUsernameError(err) {
-				ch <- fmt.Sprintf("%s is not available on %s", username, c.Name())
+			if err := c.Check(client, username); err != nil {
+				ch <- err.Error()
 			} else {
-				ch <- fmt.Sprintf("%s is available on %s", username, c.Name())
+				ch <- fmt.Sprintf("%s is available and valid on %s", username, c.Name())
 			}
 
 		}(checker)
