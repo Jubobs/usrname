231269ead8a84465e92c6be0a191b282033297aa
Julien Cretel <j.cretel@umail.ucc.ie>
2018-07-17 15:42:56 +0100

    Add timeout to default client

--- client.go
+++ client.go
@@ -2,9 +2,16 @@ package usrname
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/pkg/errors"
 )
+
+var timeout time.Duration
+
+func init() {
+	timeout = 1000 * time.Millisecond
+}
 
 func NewClient() Client {
 	return &simpleClient{}
@@ -17,11 +24,13 @@ type Client interface {
 type simpleClient struct{}
 
 func (*simpleClient) Send(req *http.Request) (int, error) {
-	res, err := http.DefaultClient.Do(req)
+	client := http.DefaultClient
+	client.Timeout = timeout
+	res, err := client.Do(req)
 	if err != nil {
 		err1 := errors.Wrap(err, "usrname: client failed")
 		return http.StatusInternalServerError, err1
 	}
-	defer res.Body.Close() // TODO: is the body nil if the method is HEAD?
-	return res.StatusCode, err
+	defer res.Body.Close()
+	return res.StatusCode, nil
 }
