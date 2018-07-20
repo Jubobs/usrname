beb427c586b3853b42b8e58f6be69287daf668a6
Julien Cretel <j.cretel@umail.ucc.ie>
2018-07-20 18:14:23 +0100

    Add support for Medium

---
+++ medium/medium.go
@@ -0,0 +1,112 @@
+package medium
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"regexp"
+	"unicode"
+
+	"github.com/jubobs/usrname"
+	"github.com/jubobs/usrname/internal"
+)
+
+type medium struct {
+	name      string
+	scheme    string
+	host      string
+	whitelist *unicode.RangeTable
+	minLength int
+	maxLength int
+}
+
+var mediumImpl = medium{
+	name:   "Medium",
+	scheme: "https",
+	host:   "medium.com",
+	whitelist: &unicode.RangeTable{
+		R16: []unicode.Range16{
+			{'.', '.', 1},
+			{'0', '9', 1},
+			{'A', 'Z', 1},
+			{'_', '_', 1},
+			{'a', 'z', 1},
+		},
+	},
+	minLength: 1,
+	maxLength: 16,
+}
+
+func New() usrname.Checker {
+	return &mediumImpl
+}
+
+func (t *medium) Name() string {
+	return t.name
+}
+
+func (t *medium) Link(username string) string {
+	u := url.URL{
+		Scheme: mediumImpl.scheme,
+		Host:   mediumImpl.host,
+		Path:   "@" + username,
+	}
+	return u.String()
+}
+
+func (t *medium) IllegalPattern() *regexp.Regexp {
+	return nil
+}
+
+func (t *medium) Whitelist() *unicode.RangeTable {
+	return t.whitelist
+}
+
+// See https://help.medium.com/en/managing-your-account/medium-username-rules
+func (t *medium) Validate(username string) []usrname.Violation {
+	return internal.CheckAll(
+		username,
+		internal.CheckLongerThan(t.minLength),
+		internal.CheckOnlyContains(t.whitelist),
+		internal.CheckShorterThan(t.maxLength),
+	)
+}
+
+func (t *medium) Check(client usrname.Client) func(string) usrname.Result {
+	return func(username string) (res usrname.Result) {
+		res.Username = username
+		res.Checker = t
+
+		if vv := t.Validate(username); len(vv) != 0 {
+			res.Status = usrname.Invalid
+			const templ = "%q is invalid on %s"
+			res.Message = fmt.Sprintf(templ, username, t.Name())
+			return
+		}
+
+		u := t.Link(username)
+		req, err := http.NewRequest("GET", u, nil)
+		statusCode, err := client.Send(req)
+		if err != nil {
+			res.Status = usrname.UnknownStatus
+			type timeout interface {
+				Timeout() bool
+			}
+			if err, ok := err.(timeout); ok && err.Timeout() {
+				res.Message = fmt.Sprintf("%s timed out", t.Name())
+			} else {
+				res.Message = "Something went wrong"
+			}
+		}
+		switch statusCode {
+		case http.StatusOK:
+			res.Status = usrname.Unavailable
+		case http.StatusNotFound:
+			res.Status = usrname.Available
+		default:
+			res.Status = usrname.UnknownStatus
+			res.Message = "Something went wrong"
+		}
+		return
+	}
+}
---
+++ medium/medium_test.go
@@ -0,0 +1,163 @@
+package medium_test
+
+import (
+	"errors"
+	"net/http"
+	"reflect"
+	"testing"
+
+	"github.com/fortytw2/leaktest"
+	"github.com/jubobs/usrname"
+	"github.com/jubobs/usrname/medium"
+	"github.com/jubobs/usrname/mock"
+)
+
+var s = medium.New()
+
+func TestName(t *testing.T) {
+	defer leaktest.Check(t)()
+	const expected = "Medium"
+	actual := s.Name()
+	if actual != expected {
+		template := "got %q, want %q"
+		t.Errorf(template, actual, expected)
+	}
+}
+
+func TestLink(t *testing.T) {
+	defer leaktest.Check(t)()
+	const username = "foobar"
+	const expected = "https://medium.com/@" + username
+	actual := s.Link(username)
+	if actual != expected {
+		template := "got %q, want %q"
+		t.Errorf(template, actual, expected)
+	}
+}
+
+func TestValidate(t *testing.T) {
+	defer leaktest.Check(t)()
+	noViolations := []usrname.Violation{}
+	cases := []struct {
+		label      string
+		username   string
+		violations []usrname.Violation
+	}{
+		{
+			"empty",
+			"",
+			[]usrname.Violation{
+				&usrname.TooShort{
+					Min:    1,
+					Actual: 0,
+				},
+			},
+		}, {
+			"onechar",
+			"0",
+			noViolations,
+		}, {
+			"exoticchars",
+			"exotic^chars",
+			[]usrname.Violation{
+				&usrname.IllegalChars{
+					At:        []int{6},
+					Whitelist: s.Whitelist(),
+				},
+			},
+		}, {
+			"toolong",
+			"01234567890123456",
+			[]usrname.Violation{
+				&usrname.TooLong{
+					Max:    16,
+					Actual: 17,
+				},
+			},
+		}, {
+			"exoticcharstoolong",
+			"0123456789^!01234",
+			[]usrname.Violation{
+				&usrname.IllegalChars{
+					At:        []int{10, 11},
+					Whitelist: s.Whitelist(),
+				},
+				&usrname.TooLong{
+					Max:    16,
+					Actual: 17,
+				},
+			},
+		},
+	}
+	const template = "Validate(%q), got %s, want %s"
+	for _, c := range cases {
+		t.Run(c.label, func(t *testing.T) {
+			if vv := s.Validate(c.username); !reflect.DeepEqual(vv, c.violations) {
+				t.Errorf(template, c.username, vv, c.violations)
+			}
+		})
+	}
+}
+
+func TestCheck(t *testing.T) {
+	defer leaktest.Check(t)()
+
+	cases := []struct {
+		label    string
+		username string
+		client   usrname.Client
+		status   usrname.Status
+	}{
+		{
+			label:    "invalid",
+			username: "_obviously_invalid!",
+			client:   nil,
+			status:   usrname.Invalid,
+		}, {
+			label:    "notfound",
+			username: "dummy",
+			client:   mock.Client(http.StatusNotFound, nil),
+			status:   usrname.Available,
+		}, {
+			label:    "ok",
+			username: "dummy",
+			client:   mock.Client(http.StatusOK, nil),
+			status:   usrname.Unavailable,
+		}, {
+			label:    "other",
+			username: "dummy",
+			client:   mock.Client(999, nil), // anything other than 200 or 404
+			status:   usrname.UnknownStatus,
+		}, {
+			label:    "clienterror",
+			username: "dummy",
+			client:   mock.Client(0, errors.New("Oh no!")),
+			status:   usrname.UnknownStatus,
+		}, {
+			label:    "timeouterror",
+			username: "dummy",
+			client:   mock.Client(0, &timeoutError{}),
+			status:   usrname.UnknownStatus,
+		},
+	}
+
+	const template = "Check(%q), got %q, want %q"
+	for _, c := range cases {
+		t.Run(c.label, func(t *testing.T) {
+			res := s.Check(c.client)(c.username)
+			actual := res.Status
+			expected := c.status
+			if actual != expected {
+				t.Errorf(template, c.username, actual, expected)
+			}
+		})
+	}
+}
+
+type timeoutError struct {
+	error
+}
+
+func (*timeoutError) Timeout() bool {
+	return true
+}
